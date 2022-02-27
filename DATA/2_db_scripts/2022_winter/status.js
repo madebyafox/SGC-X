ab1867d833e0aef53166509740c9d5c9e76d3bf6
madebyafox <amyraefox@gmail.com>
2022-02-26 17:13:14 -0800

    add sona_fail script for granting excused no shows

--- DATA/2_db_scripts/2022_winter/status.js
+++ DATA/2_db_scripts/2022_winter/status.js
@@ -3,44 +3,67 @@
 //participants per condition that have valid data
 // —————————————————————————————————————————————————————————————
 
-//CREATE A COLLECTION OF ALL SUBJECTS
+//CREATE A COLLECTION WITH STATUS BY STUDY, AND CONDITION
 db.entries.aggregate([
-    //unwind participant 
-    {$unwind: "$data"},
-    {$replaceRoot: { newRoot: "$data" }},
-    //unwind trials
-    {$unwind: "$trials"},
-    {$replaceRoot: { newRoot: "$trials" }},
-    //filter trials 
-    {$match: {block: {$in: ["participant"] }}},
-    //filter status
-    {$match: {status: {$in: ["success"] }}},
-    {$group: {
-        _id: {
-            study: "$study",
-            condition:"$condition"
-        },
-            count: { $count: {} },
-            av_discriminant: { $avg: "$discriminant_score" },
-            sd_discriminant: { $stdDevSamp: "$discriminant_score" },
-            min_discriminant: { $min: "$discriminant_score" },
-            max_discriminant: { $max: "$discriminant_score" },
-            av_absolute: { $avg: "$absolute_score" },    
-            av_tri: { $avg: "$tri_score" },    
-            av_orth: { $avg: "$orth_score" },    
-            av_other: { $avg: "$other_score" }    
-    }},
-    {$project: {
-        _id : {$concat:["$_id.study","—","$_id.condition"]},
-        count: 1,
-        av_discriminant: 1,
-        sd_discriminant: 1,
-        min_discriminant: 1,
-        max_discriminant: 1,
-        av_absolute: 1,
-        av_tri: 1,
-        av_orth: 1,
-        av_other: 1
-    }},
-    {$out: "_status"}
-]);
+        //unwind participant 
+        {$unwind: "$data"},
+        {$replaceRoot: { newRoot: "$data" }},
+        //unwind trials
+        {$unwind: "$trials"},
+        {$replaceRoot: { newRoot: "$trials" }},
+        //filter trials 
+        {$match: {block: {$in: ["participant"] }}},
+        //filter status
+        // {$match: {status: {$in: ["success"] }}},
+        {$group: {
+            _id: {
+                study: "$study",
+                condition:"$condition",
+                status: "$status"
+            },
+                count: { $count: {} },
+                av_discriminant: { $avg: "$discriminant_score" },
+                sd_discriminant: { $stdDevSamp: "$discriminant_score" },
+                min_discriminant: { $min: "$discriminant_score" },
+                max_discriminant: { $max: "$discriminant_score" },
+                av_absolute: { $avg: "$absolute_score" },    
+                av_tri: { $avg: "$tri_score" },    
+                av_orth: { $avg: "$orth_score" },    
+                av_other: { $avg: "$other_score" }    
+        }},
+        {$project: {
+            _id : {$concat:["$_id.study","—","$_id.condition","$_id.status"]},
+            count: 1,
+            av_discriminant: 1,
+            sd_discriminant: 1,
+            min_discriminant: 1,
+            max_discriminant: 1,
+            av_absolute: 1,
+            av_tri: 1,
+            av_orth: 1,
+            av_other: 1
+        }},
+        {$out: "_status"}
+    ]);
+
+
+//CREATE A COLLECTION WITH SONA STUDENTS TO GIVE EXCUSED NO-SHOWS
+db.entries.aggregate([
+        //unwind participant 
+        {$unwind: "$data"},
+        {$replaceRoot: { newRoot: "$data" }},
+        //unwind trials
+        {$unwind: "$trials"},
+        {$replaceRoot: { newRoot: "$trials" }},
+        //filter trials 
+        {$match: {block: {$in: ["participant","interaction"] }}},
+        //filter status
+        {$match: {status: {$nin: ["success"] }}},
+        {$group: {
+            _id: { $concat: ["$sona_id","-","$subject"]},
+            date : { $first : "$starttime"},
+            source: { $first : "$source"},
+            }},
+        {$out: "_sona_fail"}
+
+]);    
