0d6fcd76025008fb89aeb024d96c2ca3c4044506
Amy Rae Fox <amyraefox@gmail.com>
2022-07-20 11:40:09 -0700

    db scripts for summer data 4C

---
+++ DATA/2_db_scripts/2022_summer/flatten.js
@@ -0,0 +1,198 @@
+//FLATTEN.JS ———————————————————————————————————————————————————
+//refactor to fit new data format for SGCX
+// —————————————————————————————————————————————————————————————
+
+//STEP 0: Import all session files into the ALL_RAW collection
+//(ie. using mongoimport)
+//mongoimport --collection=ALL_RAW --db=XSPRING2018 --file=Wextra.json
+
+
+//CREATE A COLLECTION OF ALL SUBJECTS
+db.entries.aggregate(
+ [
+     //select only successful participants 
+    {$match: {"data.trials.status": {$in: ["success"] }}},  
+    {$unwind: "$data"},
+    {$replaceRoot: { newRoot: "$data" }},
+    {$out: "BY_SUBJECT"}
+]);
+
+//CREATE A COLLECTION OF ALL TRIALS
+db.BY_SUBJECT.aggregate([
+       {$unwind: "$trials"},
+       {$replaceRoot: { newRoot: "$trials" }},
+       {$out: "BY_TRIAL"}
+]);
+       
+// //COLLECTION OF PARTICIPANT-LEVEL DATA 
+// //limited by subjects who finished the study
+// db.BY_TRIAL.aggregate([  
+//   //select relevant trials
+//   {$match: {block: {$in: ["item_free", "effort", "demographics", "participant"] }}},
+//   {$project: {
+//       //every trial has subjectID
+//       subject:1,
+//       //store relevant trial level data in response object
+//       response: { $switch: {
+//         branches: [
+//             { case: {$eq: ['$block', 'participant']} , then: {
+//                 "subject" : "$subject",
+//                 "study" : "$study",
+//                 "condition" : "$condition",
+//                 "session" : "$session",
+//                 "browser" : "$browser" ,
+//                 "width" : "$width" ,
+//                 "height" : "$height" ,
+//                 "os" : "$os" ,
+//                 "starttime" : "$starttime" ,
+//                 "totaltime" : "$totaltime" ,
+//                 "violations" : "$violations" ,
+//                 "absolute_score" : "$absolute_score" ,
+//                 "discriminant_score" : "$discriminant_score" ,
+//                 "tri_score" : "$tri_score" ,
+//                 "orth_score" : "$orth_score" ,
+//                 "other_score" : "$other_score" ,
+//                 "blank_score" : "$blank_score",
+//                 "exp_id" : "$exp_id",
+//                 "sona_id" : "$sona_id",
+//                 "pool" : "$pool",
+//                 "mode" : "$mode",
+//                 "status" : "$status" }},
+//             { case: {$eq: ['$block', 'demographics']} , then: "$response" },
+//             { case: {$eq: ['$block', 'effort']} , then: "$response" },
+//             { case: {$eq: ['$block', 'item_free']} , then: {
+//                 "explanation": "$freeresponse",
+//                 //DELETE AFTER FIX PROD
+//                 "exp_id" : "$exp_id",
+//                 "sona_id" : "$sona_id",
+//                 "pool" : "$pool",
+//                 "mode" : "$mode" }}
+//          ],
+//          default: null
+//       }},     
+//   }},
+//   //merge all response objects
+//   {$group: { _id: "$subject", response: { $mergeObjects: "$response" } } },
+//   //make response root
+//   {$replaceRoot: { newRoot: "$response" }},
+//   //get rid of dummy fields
+//   {$project: {"P0_Q0":0, "P0_Q1":0, }},
+//   //set id to subject
+//   {$set: {_id : "$subject"}},
+//   {$out: "final_participants"}
+// ]);      
+
+
+// //COLLECTION OF PARTICIPANT-LEVEL DATA 
+// //limited by subjects who finished the study AND attention check
+db.entries.aggregate([  
+    //select relevant trials
+    {$match: {"data.trials.status": {$in: ["success"] }}},  
+    {$unwind: "$data"},
+    {$replaceRoot: { newRoot: "$data" }},
+    {$unwind: "$trials"},
+    {$replaceRoot: { newRoot: "$trials" }},
+    {$match: {block: {$in: ["item_free", "effort", "demographics", "participant","item_nondiscriminant"] }}},
+    {$project: {
+        //every trial has subjectID
+        subject:1,
+        //store relevant trial level data in response object
+        response: { $switch: {
+          branches: [
+              { case: {$eq: ['$block', 'participant']} , then: {
+                  "subject" : "$subject",
+                  "study" : "$study",
+                  "condition" : "$condition",
+                  "session" : "$session",
+                  "browser" : "$browser" ,
+                  "width" : "$width" ,
+                  "height" : "$height" ,
+                  "os" : "$os" ,
+                  "starttime" : "$starttime" ,
+                  "totaltime" : "$totaltime" ,
+                  "violations" : "$violations" ,
+                  "absolute_score" : "$absolute_score" ,
+                  "discriminant_score" : "$discriminant_score" ,
+                  "tri_score" : "$tri_score" ,
+                  "orth_score" : "$orth_score" ,
+                  "other_score" : "$other_score" ,
+                  "blank_score" : "$blank_score",
+                  "exp_id" : "$exp_id",
+                  "sona_id" : "$sona_id",
+                  "pool" : "$pool",
+                  "mode" : "$mode",
+                  "status" : "$status" }},
+              { case: {$eq: ['$block', 'demographics']} , then: "$response" },
+              { case: {$eq: ['$block', 'effort']} , then: "$response" },
+              { case: {$eq: ['$block', 'item_nondiscriminant']} , then: {
+                  
+                  $switch: {
+                      branches: [
+                          { case: {$eq: ['$q', 6]} , then: {
+                                  "attn_check": "$correct" }}
+                                  ], default: null}
+                  }},
+              { case: {$eq: ['$block', 'item_free']} , then: {
+                  "explanation": "$freeresponse",
+                  //DELETE AFTER FIX PROD
+                  "exp_id" : "$exp_id",
+                  "sona_id" : "$sona_id",
+                  "pool" : "$pool",
+                  "mode" : "$mode" }}
+           ],
+           default: null
+        }},     
+    }},
+    //merge all response objects
+    {$group: { _id: "$subject", response: { $mergeObjects: "$response" } } },
+    //make response root
+    {$replaceRoot: { newRoot: "$response" }},
+    //get rid of dummy fields
+    {$project: {"P0_Q0":0, "P0_Q1":0, }},
+    //set id to subject
+    {$set: {_id : "$subject"}},
+    {$out: "final_participants"}
+  ]);   
+
+//SONA RECONCILIATION LIST
+db.final_participants.aggregate([
+    {$project:{
+        _id:"$subject",
+        subject:1,
+        exp_id:1,
+        sona_id:1,
+        starttime:1,
+        absolute_score:1,
+        discriminant_score:1,
+        status: 1
+    }},
+    {$out:"final_sona_data"}
+]);
+
+//CREATE A COLLECTION OF TEST ITEMS [with mouse data]
+db.BY_TRIAL.aggregate([
+    {$match: {block: {$in: ["item_scaffold","item_nondiscriminant","item_test"] }}},
+    //supress unecessary fields
+    {$project: {
+        url: 0,
+        trial_type: 0,
+        trial_index:0,
+        internal_node_id: 0
+    }},
+    {$out: "final_items_mouse"}
+]);
+
+//CREATE A COLLECTION OF TEST ITEMS [no mouse data]
+db.final_items_mouse.aggregate([
+    //suppress mouse-related data
+    {$project: {
+        response : 0,
+        mouse_tracking_data:0,
+        mouse_tracking_targets:0,
+        mouselog:0
+    }},
+    {$out: "final_items"}
+]);
+ 
+
+
---
+++ DATA/2_db_scripts/2022_summer/status.js
@@ -0,0 +1,146 @@
+//STATUS.JS ———————————————————————————————————————————————————
+//run against database to get counts of 
+//participants per condition that have valid data
+// —————————————————————————————————————————————————————————————
+
+//CREATE A COLLECTION WITH STATUS BY STUDY, AND CONDITION
+db.entries.aggregate([
+        //unwind participant 
+        {$unwind: "$data"},
+        {$replaceRoot: { newRoot: "$data" }},
+        //unwind trials
+        {$unwind: "$trials"},
+        {$replaceRoot: { newRoot: "$trials" }},
+        //filter trials 
+        {$match: {block: {$in: ["participant"] }}},
+        {$match: {status: {$in: ["success"] }}},
+        //filter status
+        // {$match: {status: {$in: ["success"] }}},
+        {$group: {
+            _id: {
+                study: "$study",
+                condition:"$condition",
+                },
+//              n: { $count: {} } //stopped working?
+                n: { $sum: 1 },
+                avg_absolute: { $avg: "$absolute_score" },
+                sd_absolute: { $stdDevSamp: "$absolute_score" },
+                min_absolute: { $min: "$absolute_score" },
+                max_absolute: { $max: "$absolute_score" },
+                av_tri: { $avg: "$tri_score" },    
+                av_orth: { $avg: "$orth_score" },    
+                av_other: { $avg: "$other_score" }    
+         }},
+        {$project: {
+             _id : {$concat:["$_id.study","—","$_id.condition","-",{$toString:"$n"}]},
+            n: 1,
+            avg_absolute: 1,
+            sd_absolute: 1,
+            min_absolute: 1,
+            max_absolute: 1,
+            av_tri: 1,
+            av_orth: 1,
+            av_other: 1
+       }},
+        {$sort:{ _id: 1}},
+        {$out: "test_success"}
+
+    ]);
+        
+ //CREATE A COLLECTION WITH STATUS BY STUDY, AND CONDITION
+db.entries.aggregate([
+        //unwind participant 
+        {$unwind: "$data"},
+        {$replaceRoot: { newRoot: "$data" }},
+        //unwind trials
+        {$unwind: "$trials"},
+        {$replaceRoot: { newRoot: "$trials" }},
+        //filter trials 
+        {$match: {block: {$in: ["participant"] }}},
+        {$match: {status: {$nin: ["success"] }}},
+        {$group: {
+            _id: {
+                study: "$study",
+                condition:"$condition",
+                status: "$status"
+                },
+//              n: { $count: {} } //stopped working?
+                n: { $sum: 1 },
+                av_discriminant: { $avg: "$discriminant_score" },
+                sd_discriminant: { $stdDevSamp: "$discriminant_score" },
+                min_discriminant: { $min: "$discriminant_score" },
+                max_discriminant: { $max: "$discriminant_score" },
+                av_absolute: { $avg: "$absolute_score" },    
+                av_tri: { $avg: "$tri_score" },    
+                av_orth: { $avg: "$orth_score" },    
+                av_other: { $avg: "$other_score" }    
+         }},
+        {$project: {
+             _id : {$concat:["$_id.study","—","$_id.condition","-","$_id.status","-",{$toString:"$n"}]},
+            n: 1,
+            av_discriminant: 1,
+            sd_discriminant: 1,
+            min_discriminant: 1,
+            max_discriminant: 1,
+            av_absolute: 1,
+            av_tri: 1,
+            av_orth: 1,
+            av_other: 1
+       }},
+        {$sort: {_id:1}} ,
+        {$out: "test_fail"}
+    ]);
+       
+
+//CREATE A COLLECTION WITH SONA STUDENTS TO GIVE EXCUSED NO-SHOWS
+db.entries.aggregate([
+    //filter on status
+    {$match: {"data.trials.status": {$nin: ["success"] }}},
+    //unwind participant 
+    {$unwind: "$data"},
+    {$replaceRoot: { newRoot: "$data" }},
+    //unwind trials
+    {$unwind: "$trials"},
+    {$replaceRoot: { newRoot: "$trials" }},
+    //filter trials 
+    {$match: {block: {$in: ["participant","browser_check"] }}},    
+    {$project: {
+        _id: { $concat: ["SONA ID: ","$sona_id"]},
+        response: { $switch: {
+            branches: [
+                {case: {$eq: ['$block','browser_check']}, then: {
+                    "browser"  : "$browser",
+                    "mobile"  : "$mobile",
+                    "width"  : "$width",
+                    "height"  : "$height",
+                    "os"  : "$os"
+                }},
+                {case: {$eq: ['$block','participant']}, then: {
+                    "status"  : "$status",
+                    "study"  : "$study",
+                    "condition"  : "$condition",
+                    "source"  : "$source",
+                    "starttime"  : "$starttime"
+                }},
+            ],
+            default: null}}
+    }},
+    //merge all response objects
+    {$group: { _id: "$_id", response: { $mergeObjects: "$response" } } },
+    //flatten response object
+    {$project: {
+        _id: "$_id",
+        status:  "$response.status",
+        study: "$response.study",
+        condition:  "$response.condition",
+        subject:  "$response.subject",
+        starttime:  "$response.starttime",
+        os:  "$response.os",
+        browser: "$response.browser",
+        mobile:  "$response.mobile",
+        width: "$response.width",
+        height:  "$response.height"
+    }},
+    {$out: "test_sona_fail"}
+
+]);    
