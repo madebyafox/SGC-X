640a9a6c9d0d6fdbdb29bec8102fddbc2218ab6b
madebyafox <amyraefox@gmail.com>
2022-07-22 19:09:19 -0700

    rename dirs to order sessions

---
+++ DATA/3_ready_files/2_2017_fall/mouseflow_recordings/Mouseflow_0a0e0f252737b3b27f3631b906559699/data/session.js
@@ -0,0 +1,55 @@
+var MF = MF || {};MF.RecordingData = MF.RecordingData || {};MF.RecordingData["session"] = {
+  "id": "0a0e0f252737b3b27f3631b906559699",
+  "created": "2018-05-22T15:10:59.06192-07:00",
+  "lastActivity": "2018-05-22T15:11:29.48692-07:00",
+  "pageViews": [
+    {
+      "id": "052259786afea7a353e3078fbc30705014470948",
+      "startTime": "2018-05-22T15:10:59.06192-07:00",
+      "endTime": "2018-05-22T15:11:29.48692-07:00",
+      "title": "FOX2YP",
+      "uri": "https://warm-citadel-75324.herokuapp.com/",
+      "websitePage": "/15",
+      "visitTime": 30425,
+      "engagementTime": 15421,
+      "scroll": 100.0,
+      "tags": [
+        "form-interact"
+      ],
+      "friction": 0,
+      "annotations": []
+    }
+  ],
+  "duration": 30425,
+  "engagementTime": 15421,
+  "totalFriction": 0,
+  "country": "us",
+  "region": "CA",
+  "city": "La Jolla",
+  "isp": "University of California, San Diego",
+  "ip": "128.54.68.26",
+  "lang": "en-US",
+  "userAgent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
+  "browser": "Chrome",
+  "browserVersion": "66.0.3359.181",
+  "os": "Windows",
+  "osVersion": "7",
+  "device": "Desktop",
+  "referrer": "",
+  "referrerType": "",
+  "screenRes": "1920x1200",
+  "tags": [
+    "form-interact"
+  ],
+  "variables": [
+    "SID=2S99A",
+    "CONDITION=115",
+    "ORTH_CORRECT=1"
+  ],
+  "watched": false,
+  "starred": false,
+  "lng": -117.2359,
+  "lat": 32.8807,
+  "visitorId": "2923e05ebfa7849e9e2ec2ad934a054a",
+  "gdpr": false
+}
