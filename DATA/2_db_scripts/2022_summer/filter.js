0ee3ce6399a2d585e10c350b343d08f19f88515e
Amy Rae Fox <amyraefox@gmail.com>
2022-08-26 23:01:08 -0700

    commentdangerous filter file

--- DATA/2_db_scripts/2022_summer/filter.js
+++ DATA/2_db_scripts/2022_summer/filter.js
@@ -2,6 +2,8 @@
 //filter WIP dataset to include items only for specified study
 // —————————————————————————————————————————————————————————————
 
+// WARNING! SOMETHING ABOUT THIS IS DESTRUCTIVE...REMOVING ENTRIES FROM entries 
+
 //STEP 0: Import raw db files from server to local 
 //(ie. using mongoimport)
 //`mongoexport --uri mongodb+srv://[username]:[password]@[dbname].2ugwr.mongodb.net/2ypdb-s3-beh --collection entries --type JSON --out wi22_SGC3A.json`
@@ -12,56 +14,57 @@
 
 //CREATE A COLLECTION OF entries FOR GIVEN STUDY
 //EG change string match for title of desired study
-db.entries.aggregate([
-    //select relevant entries for this study
-    {$match: {'data.trials.study' : 'SGC3A'}},
-    {$out: "SGC_3A"}
-]);
-    
-db.entries.aggregate([
-    //select relevant entries for this study
-    {$match: {'data.trials.study' : 'SGC3B'}},
-    {$out: "SGC_3B"}
-]);    
-    
-db.entries.aggregate([
-    //select relevant entries for this study
-    {$match: {'data.trials.study' : 'SGC4A'}},
-    {$out: "SGC_4A"}
-]); 
 
-db.entries.aggregate([
-    //select relevant entries for this study
-    {$match: {'data.trials.study' : 'SGC4C'}},
-    {$out: "SGC_4C"}
-]); 
-    
-db.entries.aggregate([
-    //select relevant entries for this study
-    {$match: {'data.trials.study' : 'SGC4B'}},
-    {$out: "SGC_4B"}
-]);    
-
-db.entries.aggregate([
-    //select relevant entries for this study
-    {$match: {'data.trials.study' : 'SGC4D'}},
-    {$out: "SGC_4D"}
-]);        
-    
-db.entries.aggregate([
-    //select relevant entries for this study
-    {$match: {'data.trials.study' : 'SGC5A'}},
-    {$out: "SGC_5A"}
-]); 
-
-db.entries.aggregate([
-    //select relevant entries for this study
-    {$match: {'data.trials.study' : 'SGC5X'}},
-    {$out: "SGC_X"}
-]); 
-    
-    db.entries.aggregate([
-    //select relevant entries for this study
-    {$match: {'data.trials.study' : 'SGCX'}},
-    {$out: "SGC_X"}
-]); 
+// db.entries.aggregate([
+//     //select relevant entries for this study
+//     {$match: {'data.trials.study' : 'SGC3A'}},
+//     {$out: "SGC_3A"}
+// ]);
+//     
+// db.entries.aggregate([
+//     //select relevant entries for this study
+//     {$match: {'data.trials.study' : 'SGC3B'}},
+//     {$out: "SGC_3B"}
+// ]);    
+//     
+// db.entries.aggregate([
+//     //select relevant entries for this study
+//     {$match: {'data.trials.study' : 'SGC4A'}},
+//     {$out: "SGC_4A"}
+// ]); 
+// 
+// db.SGC_4C.aggregate([
+//     //select relevant entries for this study
+//     {$match: {'data.trials.study' : 'SGC4C'}},
+//     {$out: "entries"}
+// ]); 
+//     
+// db.entries.aggregate([
+//     //select relevant entries for this study
+//     {$match: {'data.trials.study' : 'SGC4B'}},
+//     {$out: "SGC_4B"}
+// ]);    
+// 
+// db.entries.aggregate([
+//     //select relevant entries for this study
+//     {$match: {'data.trials.study' : 'SGC4D'}},
+//     {$out: "SGC_4D"}
+// ]);        
+//     
+// db.entries.aggregate([
+//     //select relevant entries for this study
+//     {$match: {'data.trials.study' : 'SGC5A'}},
+//     {$out: "SGC_5A"}
+// ]); 
+// 
+// db.entries.aggregate([
+//     //select relevant entries for this study
+//     {$match: {'data.trials.study' : 'SGC5X'}},
+//     {$out: "SGC_X"}
+// ]); 
+//     
+// db.entries.aggregate([
+//     //select relevant entries for this study
+//     {$match: {'data.trials.study' : 'SGCX'}},
+//     {$out: "SGC_X"}
+// ]); 
